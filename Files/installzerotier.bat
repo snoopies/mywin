fa841a642307c2759890ae2639d2b193afe6098f
snoopies <cngz.gva@gmail.com>
2023-11-26 08:30:10 +0800

    Update installzerotier.bat

--- Files/installzerotier.bat
+++ Files/installzerotier.bat
@@ -1,2 +1,2 @@
 msiexec /i "ZeroTier One.msi" /passive
-"C:\Program Files (x86)\ZeroTier\One\zerotier-cli.bat" join "e4da7455b2f0e074"
+"C:\Program Files (x86)\ZeroTier\One\zerotier-cli.bat" join "$NETWORK_ID"
