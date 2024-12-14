e063590412433ed95e7e321f82aab65855bb14d9
snoopies <cngz.gva@gmail.com>
2024-12-14 23:47:14 +0800

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -15,7 +15,7 @@ sc config Audiosrv start= auto >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
 ping -n 10 127.0.0.1 >nul
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './Files/vnt.bat'" > out.txt 2>&1
+rem PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './Files/vnt.bat'" > out.txt 2>&1
 
 
 
